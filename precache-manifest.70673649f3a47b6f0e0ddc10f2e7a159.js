9b3344026b92498fc03bf915495285b9ba3875c8
tanishbawa <tanishbawa15@gmail.com>
2020-10-12 23:36:46 +0530

    Updates

---
+++ precache-manifest.70673649f3a47b6f0e0ddc10f2e7a159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4184ce23e669c6eb83c7dca55cfb9c9c",
+    "url": "/Budget-Calculator/index.html"
+  },
+  {
+    "revision": "78885cb27ac74471b128",
+    "url": "/Budget-Calculator/static/css/main.508cb976.chunk.css"
+  },
+  {
+    "revision": "6523e29fb44e8486294b",
+    "url": "/Budget-Calculator/static/js/2.e4de45fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-Calculator/static/js/2.e4de45fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78885cb27ac74471b128",
+    "url": "/Budget-Calculator/static/js/main.4d313c19.chunk.js"
+  },
+  {
+    "revision": "79bc5385762d5e8fcf50",
+    "url": "/Budget-Calculator/static/js/runtime-main.76e0d9c0.js"
+  }
+]);
